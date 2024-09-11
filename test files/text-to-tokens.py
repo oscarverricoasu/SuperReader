c54a2a40e9e104bb01556dff7cb23cae89f359b6
aoscr <118695045+aoscr@users.noreply.github.com>
2024-09-11 02:15:41 -0400

    Created sample pipeline for text to tokens

---
+++ test files/text-to-tokens.py
@@ -0,0 +1,16 @@
+import spacy
+import en_core_web_sm
+
+# This loads the Natural Language Processor in english (hence the "en")
+nlp = spacy.load("en_core_web_sm")
+
+
+
+input = "The quick brown fox jumped over the lazy dog."
+
+output = nlp(input)
+
+# This will print a list of all scanned words and what part of speech each work is, tokenizing the text.
+print("Tokens generated from input text: ")
+for token in output:
+    print(token.text + " " + token.pos_)
